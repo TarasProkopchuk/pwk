11d467a1ac914472e09a185c45ab148cec51fba9
s29068 <s29068@pjwstk.edu.pl>
2025-03-06 09:39:13 +0100

    test0.1

--- pwktest/src/Main.java
+++ pwktest/src/Main.java
@@ -1,6 +1,25 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+        int[] tab = {1,2,3,4,5};
+        show(filterEvenIndexOddValue(tab));
+    }
+    public static int[] filterEvenIndexOddValue (int[] array){
+        int[] array2 = new int[array.length];
+        for (int i = 0; i < array2.length; i++) {
+            if (i%2==0) {
+                if (array[i] % 2 != 0) {
+                    array2[i] = array[i];
+                }
+            }
+        }
+        return array2;
+    }
+    public static void show (int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
     }
 }

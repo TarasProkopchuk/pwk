8f967107abe47f60eb8044d2705e00af13628d70
s29068 <s29068@pjwstk.edu.pl>
2025-03-13 09:35:24 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pwktest/src/Main.java

--- pwktest/src/Main.java
+++ pwktest/src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+        int[] tba = {1,2,3,4,5};
+        show(filteeEvenIndexOddValue(tba));
+    }
+    public static int[] filteeEvenIndexOddValue(int[] array){
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+                if (i % 2 == 0 && array[i] % 2 != 0) {
+                        result[j] = array[i];
+                        j++;
+                }
+            }
+        return result;
+    }
+    public static void show(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
     }
 }

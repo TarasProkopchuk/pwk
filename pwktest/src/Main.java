a4b56e118c76fc3f7fefc00ff69119cb2dd1ad6e
s29068 <s29068@pjwstk.edu.pl>
2025-03-13 09:46:38 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	pwktest/src/Main.java

--- pwktest/src/Main.java
+++ pwktest/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] tba = {1,2,3,4,5};
+        int[] tba = {1,2,3,4,5,6};
+        int[] test = {5,5,5,5,1};
         show(filteeEvenIndexOddValue(tba));
+        System.out.println(findDominator(test));
     }
     public static int[] filteeEvenIndexOddValue(int[] array){
         int count = 0;
@@ -18,6 +20,22 @@ public class Main {
                         j++;
                 }
             }
+        return result;
+    }
+    public static int findDominator (int[] array) {
+        int[] tab = new int[array.length/2];
+        int count = 0;
+        int result =-1;
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length; j++) {
+                if (array[i] == array[j]) {
+                    count++;
+                    if (count>tab.length){
+                       result = array[i];
+                    }
+                }
+            }
+        }
         return result;
     }
     public static void show(int[] array){
